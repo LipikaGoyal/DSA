b4236282727d469c2cb3a7866469cd8a885344ee
LipikaGoyal <hellolipikagoyal@gmail.com>
2025-03-31 15:25:44 +0530

    Potd of 31st March

--- POTD/Put Marbles in Bags.cpp
+++ POTD/Put Marbles in Bags.cpp
@@ -34,3 +34,35 @@ class Solution {
     
         }
     };
+
+
+    //Solution 2 (Better)
+
+    class Solution {
+        public:
+            long long putMarbles(vector<int>& weights, int k) {
+                if(k==1 || weights.size()==k) return 0;
+                int n = weights.size();
+                vector<int> pw(n - 1, 0);
+        
+                for(int i = 0; i< weights.size()-1; i++){
+                    pw[i]=(weights[i]+weights[i+1]);
+                }
+        
+                sort(pw.begin(), pw.end());
+        
+                long long maxi = weights[0]+weights[n-1], mini = weights[0]+weights[n-1];
+                for(int i = 0; i< k-1; i++){
+                    auto p = pw[n-2-i];
+                    maxi+= p;
+                }
+        
+                for(int i = 0; i< k-1; i++){
+                    auto p = pw[i];
+                    mini+= p;
+                }
+        
+                return maxi-mini;
+        
+            }
+        };
